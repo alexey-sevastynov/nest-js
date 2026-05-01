e99697416e7f941f3ae9fdf1b45bd1198da16c29
alexseva94 <alexseva94@gmail.com>
2026-05-01 11:44:16 +0300

    fix-utc-date-range

--- src/common/lib/date.ts
+++ src/common/lib/date.ts
@@ -1,13 +1,13 @@
 export function getStartOfDay(date: Date | string) {
     const parsedDate = new Date(date);
-    parsedDate.setHours(0, 0, 0, 0);
+    parsedDate.setUTCHours(0, 0, 0, 0);
 
     return parsedDate;
 }
 
 export function getEndOfDay(date: Date | string) {
     const parsedDate = new Date(date);
-    parsedDate.setHours(23, 59, 59, 999);
+    parsedDate.setUTCHours(23, 59, 59, 999);
 
     return parsedDate;
 }
